2521d18d98fca94d624b641535e92737de5bd86b
Elio Capella Sánchez <eliocapella@gmail.com>
2016-10-17 13:34:45 +0200

    add session and passport to server

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,11 +2,14 @@
 'use strict';
 
 const mongoose = require('mongoose');
+
 let database = {};
 
 database.connect = function (next) {
     mongoose.connect('mongodb://localhost/todos', next);
 };
+
+database.connection = mongoose.connection;
 
 database.models = {};
 database.models.Todo = mongoose.model('Todo', new mongoose.Schema({
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,13 @@
 /*jslint node, es6, maxlen: 80*/
 'use strict';
 
-const express = require('express');
 const async = require('async');
-const path = require('path');
 const database = require('./database');
-
-const server = express();
-server.use('/', express.static("./frontend/build"));
-server.use('/api', require("./api"));
-server.get('*', function (req, res) {
-    res.sendFile(path.resolve('./frontend/build/index.html'));
-});
+const server = require('./server');
 
 async.series([
     database.connect,
-    function startServer(next) {
-        server.listen(3000, next);
-    }
+    server.listen
 ], function (err) {
     if (err) {
         console.log("Couldn't start server", err);
