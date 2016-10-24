0f4252bb754f7014d4efba6b7e8a5681034067c9
Elio Capella Sánchez <eliocapella@gmail.com>
2016-10-24 12:50:23 +0200

    extract frontend routes from router

---
+++ backend/src/frontend.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const express = require("express");
+
+const router = new express.Router();
+
+router.use("/", express.static("./frontend/build"));
+
+const sendFrontendEntry = (function () {
+    const path = require("path");
+    const indexHtmlPath = path.resolve("./frontend/build/index.html");
+
+    return function (req, res) {
+        res.sendFile(indexHtmlPath);
+    };
+})();
+
+const frontendRoutes = (function () {
+    const fs = require("fs");
+    const content = fs.readFileSync("./frontend/src/app.router.js", "utf8");
+    const regex = /(?:path\: \")(.*)(?:\")/g;
+
+    let routes = [];
+    let match;
+    while ((match = regex.exec(content))) {
+        routes.push(match[1]);
+    }
+    return routes;
+})();
+
+frontendRoutes.forEach((route) => {
+    router.get(route, sendFrontendEntry);
+});
+
+module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,14 @@
 
 const express = require("express");
 const session = require("./session");
-const path = require("path");
 
 const server = express();
 
 server.use(session.initialize());
 
-server.use("/", express.static("./frontend/build"));
 server.use("/api", require("./api"));
-server.get("*", function (req, res) {
-    res.sendFile(path.resolve("./frontend/build/index.html"));
-});
+server.use("/", require("./frontend"));
 
 module.exports.listen = function (next) {
     server.listen(3000, next);
-}
+};
